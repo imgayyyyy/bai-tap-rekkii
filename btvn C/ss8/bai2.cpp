ea5eae80a8e5ccb191b4ee239aea6028214ed9c7
ptdat <98038583+imgayyyyy@users.noreply.github.com>
2024-11-29 13:13:58 +0700

    bai2.cpp

--- btvn C/ss8/bai2.cpp
+++ btvn C/ss8/bai2.cpp
@@ -4,16 +4,17 @@ int main(){
 	int a[5]={3,4,5,7,8};
 	int x;
 	scanf("%d",&x);
+	int o=0;
 	for(int i=0;i<5;i++){
 		if(a[i]==x){
-			printf("vi tri phan tu trong mang la %d",);
-			break;
-		}else{
-			printf("phan tu khong co trong mang");
+			printf("vi tri phan tu trong mang la %d",i);
+			o=1;
 			break;
 		}
 	}
-	
+	if(0==0){
+		printf("phan tu khong co trong mang");
+	}
 	
 	
 	
